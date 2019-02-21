b74c1ddeea8bd494cfd5bf2b78d1a447d1963ff1
AlbaraaKhayat <46405039+AlbaraaKhayat@users.noreply.github.com>
2019-02-21 23:50:28 +0000

    Add files via upload

---
+++ data_utils.py
@@ -0,0 +1,85 @@
+import numpy as np
+import hickle as hkl
+from scipy.misc import imread 
+import zlib
+import os
+import sys
+from tqdm import tqdm
+from keras import backend as K
+from keras.preprocessing.image import Iterator
+#from datetime import datetime
+# Data generator that creates sequences for input into PredNet.
+class SequenceGenerator(Iterator):
+    def __init__(self, split, DATA_DIR, nt,
+                 batch_size=8, shuffle=False, seed=None,
+                 output_mode='error', sequence_start_mode='all', N_seq=None,
+                 data_format=K.image_data_format()):
+	self.DATA_DIR = DATA_DIR
+	splitt=split
+	print("Loading data")    
+	self.X = hkl.load('hko7_160_'+splitt+'_data.hkl')
+	sourcez = os.path.join(self.DATA_DIR,'src_'+splitt+'_list.hkl')
+#	self.X = out  # X will be like (n_images, nb_cols, nb_rows, nb_channels)/hkl.load(data_file)
+	print("Data loaded, loading sources")           
+	self.sources = hkl.load(sourcez) # source for each image so when creating sequences can assure that consecutive frames are from same video
+	print("Sources loaded")           
+	self.nt = nt
+        self.batch_size = batch_size
+        self.data_format = data_format
+        assert sequence_start_mode in {'all', 'unique'}, 'sequence_start_mode must be in {all, unique}'
+        self.sequence_start_mode = sequence_start_mode
+        assert output_mode in {'error', 'prediction'}, 'output_mode must be in {error, prediction}'
+        self.output_mode = output_mode
+
+        if self.data_format == 'channels_first':
+            print("transpose triggered")
+            self.X = np.transpose(self.X, (0, 3, 1, 2))
+        self.im_shape = self.X[0].shape
+
+        if self.sequence_start_mode == 'all':  # allow for any possible sequence, starting from any frame
+            self.possible_starts = np.array([i for i in range(self.X.shape[0] - self.nt) if self.sources[i] == self.sources[i + self.nt - 1]])
+        elif self.sequence_start_mode == 'unique':  #create sequences where each unique frame is in at most one sequence
+            curr_location = 0
+            possible_starts = []
+            while curr_location < self.X.shape[0] - self.nt + 1:
+                if self.sources[curr_location] == self.sources[curr_location + self.nt - 1]:
+                    possible_starts.append(curr_location)
+                    curr_location += self.nt
+                else:
+                    curr_location += 1
+            self.possible_starts = possible_starts
+
+        if shuffle:
+            self.possible_starts = np.random.permutation(self.possible_starts)
+        if N_seq is not None and len(self.possible_starts) > N_seq:  # select a subset of sequences if want to
+            self.possible_starts = self.possible_starts[:N_seq]
+        self.N_sequences = len(self.possible_starts)
+        super(SequenceGenerator, self).__init__(len(self.possible_starts), batch_size, shuffle, seed)
+
+    def __getitem__(self, null):
+        return self.next()
+
+    def next(self):
+        with self.lock:
+            current_index = (self.batch_index * self.batch_size) % self.n
+            index_array, current_batch_size = next(self.index_generator), self.batch_size
+        batch_x = np.zeros((current_batch_size, self.nt) + self.im_shape, np.float32)
+        for i, idx in enumerate(index_array):
+            idx = self.possible_starts[idx]
+            batch_x[i] = self.preprocess(self.X[idx:idx+self.nt])
+        if self.output_mode == 'error':  # model outputs errors, so y should be zeros
+            batch_y = np.zeros(current_batch_size, np.float32)
+        elif self.output_mode == 'prediction':  # output actual pixels
+            batch_y = batch_x
+        return batch_x, batch_y
+
+    def preprocess(self, X):
+        return X.astype(np.float32) / 255
+
+        
+    def create_all(self):
+        X_all = np.zeros((self.N_sequences, self.nt) + self.im_shape, np.float32)
+        for i, idx in enumerate(self.possible_starts):
+            X_all[i] = self.preprocess(self.X[idx:idx+self.nt])
+#            print("SG_fin",i)
+        return X_all
---
+++ kitti_evaluate.py
@@ -0,0 +1,93 @@
+'''
+Evaluate trained PredNet on KITTI sequences.
+Calculates mean-squared error and plots predictions.
+'''
+
+import os
+import numpy as np
+from six.moves import cPickle
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+from datetime import datetime
+from keras import backend as K
+from keras.models import Model, model_from_json
+from keras.layers import Input, Dense, Flatten
+from mnist_pred_rgcLSTM import Pred_rgcLSTM
+#from prednet import PredNet
+from data_utils import SequenceGenerator
+import hickle as hkl 
+
+n_plot = 40
+batch_size = 1
+nt = 10
+
+RESULTS_SAVE_DIR="/home/research/Dissertation/prednet-master/hko_results/"
+WEIGHTS_DIR="/home/research/Dissertation/prednet-master/hko_weights/"
+weights_file = os.path.join(WEIGHTS_DIR, 'pred_rgcLSTM_hko7_weights.hdf5')
+json_file = os.path.join(WEIGHTS_DIR, 'pred_rgcLSTM_hko7_model.json')
+split='valid' #valid,test or train
+#sourcez = os.path.join(DATA_DIR,'src_'+split+'_list.hkl')
+DATA_DIR="./"
+datetime.utcnow()
+# Load trained model
+f = open(json_file, 'r')
+json_string = f.read()
+f.close()
+train_model = model_from_json(json_string, custom_objects = {'Pred_rgcLSTM': Pred_rgcLSTM})
+train_model.load_weights(weights_file)
+
+# Create testing model (to output predictions)
+layer_config = train_model.layers[1].get_config()
+layer_config['output_mode'] = 'prediction'
+data_format = layer_config['data_format'] if 'data_format' in layer_config else layer_config['dim_ordering']
+test_prednet = Pred_rgcLSTM(weights=train_model.layers[1].get_weights(), **layer_config)
+input_shape = list(train_model.layers[0].batch_input_shape[1:])
+input_shape[0] = nt
+inputs = Input(shape=tuple(input_shape))
+predictions = test_prednet(inputs)
+test_model = Model(inputs=inputs, outputs=predictions)
+
+test_generator = SequenceGenerator(split, DATA_DIR, nt, sequence_start_mode='unique', data_format=data_format)
+X_test = test_generator.create_all()
+X_hat = test_model.predict(X_test, batch_size)
+print("X_hat made")
+if data_format == 'channels_first':
+    X_test = np.transpose(X_test, (0, 1, 3, 4, 2))
+    print("transpose trig")
+    X_hat = np.transpose(X_hat, (0, 1, 3, 4, 2))
+
+# Compare MSE of PredNet predictions vs. using last frame.  Write results to prediction_scores.txt
+print("Eval start")
+mse_model = np.mean( (X_test[:, 1:] - X_hat[:, 1:])**2 )  # look at all timesteps except the first
+mse_prev = np.mean( (X_test[:, :-1] - X_test[:, 1:])**2 )
+if not os.path.exists(RESULTS_SAVE_DIR): os.mkdir(RESULTS_SAVE_DIR)
+f = open(RESULTS_SAVE_DIR + 'prediction_scores.txt', 'w')
+f.write("Model MSE: %f\n" % mse_model)
+f.write("Previous Frame MSE: %f" % mse_prev)
+f.close()
+hkl.dump(X_test,'X_test.hkl',mode='w')
+hkl.dump(X_hat,'X_hat.hkl',mode='w')
+# Plot some predictions
+aspect_ratio = float(X_hat.shape[2]) / X_hat.shape[3]
+plt.figure(figsize = (nt, 2*aspect_ratio))
+gs = gridspec.GridSpec(2, nt)
+gs.update(wspace=0., hspace=0.)
+plot_save_dir = os.path.join(RESULTS_SAVE_DIR, 'prediction_plots/')
+if not os.path.exists(plot_save_dir): os.mkdir(plot_save_dir)
+plot_idx = np.random.permutation(X_test.shape[0])[:n_plot]
+for i in plot_idx:
+    for t in range(nt):
+        plt.subplot(gs[t])
+        plt.imshow(X_test[i,t].squeeze(), interpolation='none')
+        plt.tick_params(axis='both', which='both', bottom='off', top='off', left='off', right='off', labelbottom='off', labelleft='off')
+        if t==0: plt.ylabel('Actual', fontsize=10)
+
+        plt.subplot(gs[t + nt])
+        plt.imshow(X_hat[i,t].squeeze(), interpolation='none')
+        plt.tick_params(axis='both', which='both', bottom='off', top='off', left='off', right='off', labelbottom='off', labelleft='off')
+        if t==0: plt.ylabel('Predicted', fontsize=10)
+
+    plt.savefig(plot_save_dir +  'plot_' + str(i) + '.png')
+    plt.clf()
