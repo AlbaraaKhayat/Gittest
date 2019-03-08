979a0bd0a943406ec301a4dad283381f08d93287
AlbaraaKhayat <46405039+AlbaraaKhayat@users.noreply.github.com>
2019-03-08 23:39:28 +0000

    Update eval.py

--- eval.py
+++ eval.py
@@ -1,84 +1,122 @@
-#AlbaraaKhayat,2019.In fulfillment of MRes.
+#AlbaraaKhayat,2019.In fulfiframesment of MRes.
 import numpy as np
 import hickle as hkl
 from skimage import measure as evaluu
 from tqdm import tqdm
-#Load
-set1='X_hat.hkl'
-set2='X_test.hkl'
-hat=hkl.load(set1)
-val=hkl.load(set2)
-hat=hat*255
-val=hkl.load(set2)
-#Init
-ll=9
-lenn=len(hat)
-thresh=0.0509 #rain thresh in pixel value
-zz=np.zeros(ll)
-mse=np.zeros(ll)
-mae=np.zeros(ll)
-ssim=np.zeros(ll)
-nse=np.zeros(ll)
-std_hat=np.zeros(ll)
-std_val=np.zeros(ll)
-mse_p=np.zeros(ll)
-mae_p=np.zeros(ll)
-ssim_p=np.zeros(ll)
-nse_p=np.zeros(ll)
-std_p=np.zeros(ll)
-rmsd=np.zeros(ll)
-rmsd_p=np.zeros(ll)
-TP=np.zeros(ll)
-FP=np.zeros(ll)
-TN=np.zeros(ll)
-FN=np.zeros(ll)
-TPm=np.zeros((lenn,ll))
-FPm=np.zeros((lenn,ll))
-TNm=np.zeros((lenn,ll))
-FNm=np.zeros((lenn,ll))
-#Calc
-for i in tqdm(range(1,10)):
-    mse[i-1]=np.mean((hat[:,i]-val[:,i])**2)
-    print(mse)
-    mae[i-1]=np.mean(np.abs(val[:,i]-hat[:,i]))
-    ssim[i-1]=evaluu.compare_ssim(val[:,i],hat[:,i],win_size=3,multichannel=True)
-    nse[i-1]=1-(np.sum((hat[:,i]-val[:,i])**2)/np.sum((val[:,i]-np.mean(val[:,i]))**2))
-    std_hat[i-1]=np.std(hat[:,i])
-    std_val[i-1]=np.std(val[:,i])
-    rmsd[i-1]=np.sqrt(np.sum(np.square(hat[:,i]-val[:,i]))/25600)
-    mse_p[i-1]=np.mean((val[:,i-1]-val[:,i])**2)
-    mae_p[i-1]=np.mean(np.abs(val[:,i]-val[:,i-1]))
-    ssim_p[i-1]=evaluu.compare_ssim(val[:,i],val[:,i-1],win_size=3,multichannel=True)
-    nse_p[i-1]=1-(np.sum((val[:,i-1]-val[:,i])**2)/np.sum((val[:,i]-np.mean(val[:,i]))**2))
-    std_p[i-1]=np.std(val[:,i-1])
-    rmsd_p[i-1]=np.sqrt(np.sum(np.square(hat[:,i]-val[:,i]))/25600)
 
-    for z in range(20):
-        for x in range(160):
-            for y in range(160):
-                if hat[z,i,x,y,0] >= thresh and val[z,i,x,y,0] >= thresh:
+#LOAD
+prediction=hkl.load('X_hat.hkl')
+observation=hkl.load('X_test.hkl')
+
+#INIT
+frames=9
+#sequences=len(prediction)
+sequences=20
+threshold=0.0509 #Rain threshold in pixel value
+width=160
+height=160
+area=width*height
+
+xmse=np.zeros((sequences,frames))
+xmae=np.zeros((sequences,frames))
+xssim=np.zeros((sequences,frames))
+xnse=np.zeros((sequences,frames))
+xstd_prediction=np.zeros((sequences,frames))
+xstd_observation=np.zeros((sequences,frames))
+xmse_p=np.zeros((sequences,frames))
+xmae_p=np.zeros((sequences,frames))
+xssim_p=np.zeros((sequences,frames))
+xnse_p=np.zeros((sequences,frames))
+xstd_p=np.zeros((sequences,frames))
+xrmsd=np.zeros((sequences,frames))
+xrmsd_p=np.zeros((sequences,frames))
+
+mse=np.zeros(frames)
+mae=np.zeros(frames)
+ssim=np.zeros(frames)
+nse=np.zeros(frames)
+std_prediction=np.zeros(frames)
+std_observation=np.zeros(frames)
+mse_p=np.zeros(frames)
+mae_p=np.zeros(frames)
+ssim_p=np.zeros(frames)
+nse_p=np.zeros(frames)
+std_p=np.zeros(frames)
+rmsd=np.zeros(frames)
+rmsd_p=np.zeros(frames)
+
+TP=np.zeros(frames)
+FP=np.zeros(frames)
+TN=np.zeros(frames)
+FN=np.zeros(frames)
+TPm=np.zeros((sequences,frames))
+FPm=np.zeros((sequences,frames))
+TNm=np.zeros((sequences,frames))
+FNm=np.zeros((sequences,frames))
+
+#CALC
+
+#prediction*=255#offset normalization
+#observation*=255
+#prediction*=255#pixel to rainfall 
+#observation*=25
+
+
+for i in tqdm(range(1,10)):
+    for z in range(sequences):
+        xmse[z,i-1]=np.mean((prediction[z,i]-observation[z,i])**2)
+        xmae[z,i-1]=np.mean(np.abs(observation[z,i]-prediction[z,i]))
+        xssim[z,i-1]=evaluu.compare_ssim(observation[z,i],prediction[z,i],win_size=3,multichannel=True)
+        xnse[z,i-1]=1-(np.sum((prediction[z,i]-observation[z,i])**2)/np.sum((observation[z,i]-np.mean(observation[z,i]))**2))
+        xstd_prediction[z,i-1]=np.std(prediction[z,i])
+        xstd_observation[z,i-1]=np.std(observation[z,i])
+        xrmsd[z,i-1]=np.sqrt(np.sum(np.square(prediction[z,i]-observation[z,i]))/area)
+        xmse_p[z,i-1]=np.mean((observation[z,i-1]-observation[z,i])**2)
+        xmae_p[z,i-1]=np.mean(np.abs(observation[z,i]-observation[z,i-1]))
+        xssim_p[z,i-1]=evaluu.compare_ssim(observation[z,i],observation[z,i-1],win_size=3,multichannel=True)
+        xnse_p[z,i-1]=1-(np.sum((observation[z,i-1]-observation[z,i])**2)/np.sum((observation[z,i]-np.mean(observation[z,i]))**2))
+        xstd_p[z,i-1]=np.std(observation[z,i-1])
+        xrmsd_p[z,i-1]=np.sqrt(np.sum(np.square(prediction[z,i]-observation[z,i]))/area)
+        for x in range(width):
+            for y in range(height):
+                if prediction[z,i,x,y,0] >= threshold and observation[z,i,x,y,0] >= threshold:
                   TPm[z,i-1]+=1
-                if hat[z,i,x,y,0] >= thresh and val[z,i,x,y,0] < thresh:
+                elif prediction[z,i,x,y,0] >= threshold and observation[z,i,x,y,0] < threshold:
                   FPm[z,i-1]+=1
-                if hat[z,i,x,y,0] < thresh and val[z,i,x,y,0] < thresh:
+                elif prediction[z,i,x,y,0] < threshold and observation[z,i,x,y,0] < threshold:
                   TNm[z,i-1]+=1
-                if hat[z,i,x,y,0] < thresh and val[z,i,x,y,0] >= thresh:
+                elif prediction[z,i,x,y,0] < threshold and observation[z,i,x,y,0] >= threshold:
                   FNm[z,i-1]+=1
                 else:
                   print('Error:FP')
-        print(TPm[z])
+        if (TPm[z,i-1]+FNm[z,i-1]+TNm[z,i-1]+FPm[z,i-1]) != area:
+           print('T-F/P-N inconsistent')
     TP[i-1]=np.mean(TPm[:,i-1])
     TN[i-1]=np.mean(TNm[:,i-1])
     FP[i-1]=np.mean(FPm[:,i-1])
     FN[i-1]=np.mean(FNm[:,i-1])
-#Write for line in
-f=open('normmodel_scores.txt','w')
+    mse[i-1]=np.mean(xmse[:,i-1])
+    mae[i-1]=np.mean(xmae[:,i-1])
+    ssim[i-1]=np.mean(xssim[:,i-1])
+    nse[i-1]=np.mean(xnse[:,i-1])
+    std_prediction[i-1]=np.mean(xstd_prediction[:,i-1])
+    std_observation[i-1]=np.mean(xstd_observation[:,i-1])
+    mse_p[i-1]=np.mean(xmse_p[:,i-1])
+    mae_p[i-1]=np.mean(xmae_p[:,i-1])
+    ssim_p[i-1]=np.mean(xssim_p[:,i-1])
+    nse_p[i-1]=np.mean(xnse_p[:,i-1])
+    std_p[i-1]=np.mean(xstd_p[:,i-1])
+    rmsd[i-1]=np.mean(xrmsd[:,i-1])
+    rmsd_p[i-1]=np.mean(xrmsd_p[:,i-1])
+    
+#WRITE
+f=open('normalized_clean_scores.txt','w')
 f.write("Model MSE:%s\n" % mse)
 f.write("Model MAE:%s\n" % mae)
 f.write("Model SSIM:%s\n" % ssim)
 f.write("Model NSE:%s\n" % nse)
-f.write("Observation Stddev:%s\n" % std_val)
-f.write("Model Stddev:%s\n" % std_hat)
+f.write("Observation Stddev:%s\n" % std_observation)
+f.write("Model Stddev:%s\n" % std_prediction)
 f.write("Previous Frame Stddev:%s\n" % std_p)
 f.write("Model RMSD:%s\n" % rmsd)
 f.write("Previous Frame MSE:%s\n" % mse_p)
@@ -87,7 +125,7 @@ f.write("Previous Frame SSIM:%s\n" % ssim_p)
 f.write("Previous Frame NSE:%s\n" % nse_p)
 f.write("Previous Frame RMSD:%s\n" % rmsd_p)
 f.close()
-f=open('normmodel_PN.txt','w')
+f=open('normalized_clean_pn.txt','w')
 f.write("Model TP:%s\n" % TP)
 f.write("Model FP:%s\n" % FP)
 f.write("Model TN:%s\n" % TN)
