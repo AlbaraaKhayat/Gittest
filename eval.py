081fccd179be590acb631d82539aabad73a946f9
AlbaraaKhayat <46405039+AlbaraaKhayat@users.noreply.github.com>
2019-03-08 19:10:14 +0000

    Update eval.py

--- eval.py
+++ eval.py
@@ -9,27 +9,33 @@ set1='X_hat.hkl'
 set2='X_test.hkl'
 hat=hkl.load(set1)
 val=hkl.load(set2)
-
+#hat=hat1[:6]
+#val=hat1[:6]
 #Init
 ll=9
-np.zeros(ll)
-mse=np.zeros(ll)
-mae=np.zeros(ll)
-ssim=np.zeros(ll)
-nse=np.zeros(ll)
-std_hat=np.zeros(ll)
-std_val=np.zeros(ll)
-mse_p=np.zeros(ll)
-mae_p=np.zeros(ll)
-ssim_p=np.zeros(ll)
-nse_p=np.zeros(ll)
-std_p=np.zeros(ll)
-TP=np.zeros(ll)
-FP=np.zeros(ll)
-TN=np.zeros(ll)
-FN=np.zeros(ll)
-
-
+lenn=len(hat)
+zeros=np.zeros(ll)
+mse=zeros
+mae=zeros
+ssim=zeros
+nse=zeros
+std_hat=zeros
+std_val=zeros
+mse_p=zeros
+mae_p=zeros
+ssim_p=zeros
+nse_p=zeros
+std_p=zeros
+rmsd=zeros
+rmsd_p=zeros
+TP=zeros
+FP=zeros
+TN=zeros
+FN=zeros
+TPm=np.zeros(((lenn,ll)))
+FPm=np.zeros((lenn,ll))
+TNm=np.zeros((lenn,ll))
+FNm=np.zeros((lenn,ll))
 #Calc
 for i in tqdm(range(1,10)):
     mse[i-1]=np.mean((hat[:,i]-val[:,i])**2)
@@ -38,44 +44,47 @@ for i in tqdm(range(1,10)):
     nse[i-1]=1-(np.sum((hat[:,i]-val[:,i])**2)/np.sum((val[:,i]-np.mean(val[:,i]))**2))
     std_hat[i-1]=np.std(hat[:,i])
     std_val[i-1]=np.std(val[:,i])
-    rmsd=np.sqrt(np.sum(np.square(hat[:,i]-val[:,i]))/25600)
+    rmsd[i-1]=np.sqrt(np.sum(np.square(hat[:,i]-val[:,i]))/25600)
     mse_p[i-1]=np.mean((val[:,i-1]-val[:,i])**2)
     mae_p[i-1]=np.mean(np.abs(val[:,i]-val[:,i-1]))
     ssim_p[i-1]=evaluu.compare_ssim(val[:,i],val[:,i-1],win_size=3,multichannel=True)
     nse_p[i-1]=1-(np.sum((val[:,i-1]-val[:,i])**2)/np.sum((val[:,i]-np.mean(val[:,i]))**2))
     std_p[i-1]=np.std(val[:,i-1])
-    rmsd_p=np.sqrt(np.sum(np.square(hat[:,i]-val[:,i]))/25600)
+    rmsd_p[i-1]=np.sqrt(np.sum(np.square(hat[:,i]-val[:,i]))/25600)
     for z in range(len(hat)):
         for x in range(160):
             for y in range(160):
                 if hat[z,i,x,y]>=12.978 and val[z,i,x,y]>=12.978:
-                  TP[i]+=1
+                  TPm[z,i-1]+=1
                 if hat[z,i,x,y]>=12.978 and val[z,i,x,y]<12.978:
-                  FP[i]+=1
+                  FPm[z,i-1]+=1
                 if hat[z,i,x,y]<12.978 and val[z,i,x,y]<12.978:
-                  TN[i]+=1
+                  TNm[z,i-1]+=1
                 if hat[z,i,x,y]<12.978 and val[z,i,x,y]>=12.978:
-                  FN[i]+=1
-
-#Write
+                  FNm[z,i-1]+=1
+    TP[i-1]=np.mean(TPm[:,i-1])
+    TN[i-1]=np.mean(TNm[:,i-1])
+    FP[i-1]=np.mean(FPm[:,i-1])
+    FN[i-1]=np.mean(FNm[:,i-1])
+#Write for line in
 f=open('eval_scores.text','w')
-f.write("Model MSE:%f\n"%mse)
-f.write("Model MAE:%f\n"%mae)
-f.write("Model SSIM:%f\n"%ssim)
-f.write("Model NSE:%f\n"%nse)
-f.write("Model Stddev:%f\n"%std_hat)
-f.write("Model RMSD:%f\n"%rmsd)
-f.write("Observation Stddev:%f\n"%std_val)
-f.write("Previous Frame MSE:%f\n"%mse_p)
-f.write("Previous Frame MAE:%f\n"%mae_p)
-f.write("Previous Frame SSIM:%f\n"%ssim_p)
-f.write("Previous Frame NSE:%f\n"%nse_p)
-f.write("Previous Frame Stddev:%f\n"%std_p)
-f.write("Previous Frame RMSD:%f\n"%rmsd_p)
+f.write("Model MSE:%s\n" % (mse))
+f.write("Model MAE:%s\n" % (mae))
+f.write("Model SSIM:%s\n" % (ssim))
+f.write("Model NSE:%s\n" % (nse))
+f.write("Model Stddev:%s\n" % (std_hat))
+f.write("Model RMSD:%s\n" % (rmsd))
+f.write("Observation Stddev:%s\n" % (std_val))
+f.write("Previous Frame MSE:%s\n" % (mse_p))
+f.write("Previous Frame MAE:%s\n" % (mae_p))
+f.write("Previous Frame SSIM:%s\n" % (ssim_p))
+f.write("Previous Frame NSE:%s\n" % (nse_p))
+f.write("Previous Frame Stddev:%s\n" % (std_p))
+f.write("Previous Frame RMSD:%s\n" % (rmsd_p))
 f.close()
 f=open('eval_PN.text','w')
-f.write("Model TP:%f\n"%TP)
-f.write("Model FP:%f\n"%FP)
-f.write("Model TN:%f\n"%TN)
-f.write("Model FN:%f\n"%FN)
+f.write("Model TP:%s\n"%TP)
+f.write("Model FP:%s\n"%FP)
+f.write("Model TN:%s\n"%TN)
+f.write("Model FN:%s\n"%FN)
 f.close()
