d5ff2fc704c8fb5fc34620bcb95b87c45b51a660
AlbaraaKhayat <46405039+AlbaraaKhayat@users.noreply.github.com>
2019-03-08 02:13:47 +0000

    Update eval.py

--- eval.py
+++ eval.py
@@ -1,11 +1,24 @@
 import numpy as np
 import hickle as hkl
-import sys
 from skimage import measure as evaluu
+#Load
 set1='X_hat.hkl'
 set2='X_test.hkl'
 hat=hkl.load(set1)
 val=hkl.load(set2)
+#Init
+mse=[]
+mae=[]
+ssim=[]
+nse=[]
+std_hat=[]
+std_val=[]
+mse_p=[]
+mae_p=[]
+ssim_p=[]
+nse_p=[]
+std_p=[]
+# Calc
 for x in range(1,11):
   mse[x-1] = np.mean( (hat[:, x] - val[:, x])**2 )  
   mae[x-1] = np.mean(np.abs(val[:, x] - hat[:, x]))
@@ -20,18 +33,17 @@ for x in range(1,11):
   ssim_p[x-1]= evaluu.compare_ssim(val[:, x],val[:, x-1],win_size=3,multichannel=True)
   nse_p[x-1] = 1 - (np.sum((val[:,x-1] - val[:,x])**2)/np.sum((val[:,x] - np.mean(val[:,x]))**2))
   std_p[x-1]=np.std(val[:,x-1])
-  
-  
+
 f = open('eval_scores.txt', 'w')
 f.write("Model MSE: %f\n" % mse)
 f.write("Model MAE: %f\n" % mae)
-f.write("Model SSIM: %f\n" %ssim )
+f.write("Model SSIM: %f\n" % ssim )
 f.write("Model NSE: %f\n" % nse)
 f.write("Model Stddev: %f\n" % std_hat)
 f.write("Observation Stddev: %f\n" % std_val)
 f.write("Previous Frame MSE: %f\n" % mse_p)
 f.write("Previous Frame MAE: %f\n" % mae_p)
-f.write("Previous Frame SSIM: %f\n" %ssim_p )
+f.write("Previous Frame SSIM: %f\n" % ssim_p )
 f.write("Previous Frame NSE: %f\n" % nse_p)
 f.write("Previous Frame Stddev: %f\n" % std_p)
 f.close()
