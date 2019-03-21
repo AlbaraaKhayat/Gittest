0fed87e15efdecaee8b74cf34fc98f258a0fc0a9
AlbaraaKhayat <46405039+AlbaraaKhayat@users.noreply.github.com>
2019-03-21 17:32:26 +0000

    Update eval_mds.py

--- eval_mds.py
+++ eval_mds.py
@@ -11,8 +11,8 @@ observation=hkl.load('X_test.hkl')
 ss='rates_mds'
 
 #INIT
-frames=29
-#start=1
+frames=10
+start=20
 sequences=len(prediction)
 #sequences=20
 threshold=0.5 #0.5mm/h Rain threshold in (normalized)pixel value=0.330588,13.00365 dBZ,
@@ -90,56 +90,64 @@ observation=fixframes(observation)
 prediction=pix2rate(prediction)
 observation=pix2rate(observation)
 
-for i in tqdm(range(1,30)):
+for i in tqdm(range(20,30)):
     if i<5: previous_frame=0
     if i>=5 and i<10: previous_frame=4
     if i>=10 and i<15: previous_frame=9
     if i>=15 and i<20: previous_frame=14
     if i>=20: previous_frame=19
     for z in range(sequences):
-        xmae[z,i-1]=np.mean(np.abs(observation[z,i]-prediction[z,i]))
-        xssim[z,i-1]=evaluu.compare_ssim(observation[z,i],prediction[z,i],win_size=3,multichannel=True)
-        xstd_prediction[z,i-1]=np.std(prediction[z,i])
-        xstd_observation[z,i-1]=np.std(observation[z,i])
-        xmae_p[z,i-1]=np.mean(np.abs(observation[z,i]-observation[z,previous_frame]))
-        xssim_p[z,i-1]=evaluu.compare_ssim(observation[z,i],observation[z,previous_frame],win_size=3,multichannel=True)
+        xmae[z,i-start]=np.mean(np.abs(observation[z,i]-prediction[z,i]))
+        xssim[z,i-start]=evaluu.compare_ssim(observation[z,i],prediction[z,i],win_size=3,multichannel=True)
+        xstd_prediction[z,i-start]=np.std(prediction[z,i])
+        xstd_observation[z,i-start]=np.std(observation[z,i])
+        xmae_p[z,i-start]=np.mean(np.abs(observation[z,i]-observation[z,previous_frame]))
+        xssim_p[z,i-start]=evaluu.compare_ssim(observation[z,i],observation[z,previous_frame],win_size=3,multichannel=True)
         for x in range(width):
             for y in range(height):
                 if prediction[z,i,x,y,0] >= threshold and observation[z,i,x,y,0] >= threshold:
-                  TPm[z,i-1]+=1
+                  TPm[z,i-start]+=1
                 elif prediction[z,i,x,y,0] >= threshold and observation[z,i,x,y,0] < threshold:
-                  FPm[z,i-1]+=1
+                  FPm[z,i-start]+=1
                 elif prediction[z,i,x,y,0] < threshold and observation[z,i,x,y,0] < threshold:
-                  TNm[z,i-1]+=1
+                  TNm[z,i-start]+=1
                 elif prediction[z,i,x,y,0] < threshold and observation[z,i,x,y,0] >= threshold:
-                  FNm[z,i-1]+=1
-                elif observation[z,previous_frame,x,y,0] >= threshold and observation[z,i,x,y,0] >= threshold:
-                  pTPm[z,i-1]+=1
+                  FNm[z,i-start]+=1
+                if observation[z,previous_frame,x,y,0] >= threshold and observation[z,i,x,y,0] >= threshold:
+                  pTPm[z,i-start]+=1
                 elif observation[z,previous_frame,x,y,0] >= threshold and observation[z,i,x,y,0] < threshold:
-                  pFPm[z,i-1]+=1
+                  pFPm[z,i-start]+=1
                 elif observation[z,previous_frame,x,y,0] < threshold and observation[z,i,x,y,0] < threshold:
-                  pTNm[z,i-1]+=1
+                  pTNm[z,i-start]+=1
                 elif observation[z,previous_frame,x,y,0] < threshold and observation[z,i,x,y,0] >= threshold:
-                  pFNm[z,i-1]+=1
+                  pFNm[z,i-start]+=1
                 else:
                   print('Error:FP')
-        if (TPm[z,i-1]+FNm[z,i-1]+TNm[z,i-1]+FPm[z,i-1]) != area:
+        if (TPm[z,i-start]+FNm[z,i-start]+TNm[z,i-start]+FPm[z,i-start]) != area:
            print('T-F/P-N inconsistent')
-        elif (pTPm[z,i-1]+pFNm[z,i-1]+pTNm[z,i-1]+pFPm[z,i-1]) != area:
+        elif (pTPm[z,i-start]+pFNm[z,i-start]+pTNm[z,i-start]+pFPm[z,i-start]) != area:
            print('T-F/P-N prevframe inconsistent')
-    TP[i-1]=np.mean(TPm[:,i-1])
-    TN[i-1]=np.mean(TNm[:,i-1])
-    FP[i-1]=np.mean(FPm[:,i-1])
-    FN[i-1]=np.mean(FNm[:,i-1])
-    mae[i-1]=np.mean(xmae[:,i-1])
-    ssim[i-1]=np.mean(xssim[:,i-1])
-    std_prediction[i-1]=np.mean(xstd_prediction[:,i-1])
-    std_observation[i-1]=np.mean(xstd_observation[:,i-1])
-    mae_p[i-1]=np.mean(xmae_p[:,i-1])
-    ssim_p[i-1]=np.mean(xssim_p[:,i-1])
-
+	   zz=pTPm[z,i-start]+pFNm[z,i-start]+pTNm[z,i-start]+pFPm[z,i-start]
+	   print(zz)
+    TP[i-start]=np.mean(TPm[:,i-start])
+    TN[i-start]=np.mean(TNm[:,i-start])
+    FP[i-start]=np.mean(FPm[:,i-start])
+    FN[i-start]=np.mean(FNm[:,i-start])
+    mae[i-start]=np.mean(xmae[:,i-start])
+    ssim[i-start]=np.mean(xssim[:,i-start])
+    std_prediction[i-start]=np.mean(xstd_prediction[:,i-start])
+    std_observation[i-start]=np.mean(xstd_observation[:,i-start])
+    mae_p[i-start]=np.mean(xmae_p[:,i-start])
+    ssim_p[i-start]=np.mean(xssim_p[:,i-start])
+    pTP[i-start]=np.mean(pTPm[:,i-start])
+    pTN[i-start]=np.mean(pTNm[:,i-start])
+    pFP[i-start]=np.mean(pFPm[:,i-start])
+    pFN[i-start]=np.mean(pFNm[:,i-start])
 
 #WRITE    
+hkl.dump('test1.hkl',observation[:100])
+hkl.dump('hat1.hkl',prediction[:100])
+
 f=open(ss+'_scores.txt','w')
 f.write("Model MAE:%s\n" % mae)
 f.write("Model SSIM:%s\n" % ssim)
